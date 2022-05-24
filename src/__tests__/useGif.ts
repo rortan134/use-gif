2b35edb0db5167deeb31ea5f37b661846b1f27e1
Rortan <samaritanojr006@gmail.com>
2022-05-24 20:53:55 +0200

    setup tests

---
+++ src/__tests__/useGif.ts
@@ -0,0 +1,21 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import { defaultOptions, useGif } from "../useGif";
+
+describe("useGif", () => {
+    jest.useFakeTimers();
+
+    const el = document.createElement("div");
+    const target = { current: el };
+
+    const renderHelper = ({ ref = target, ...rest }) =>
+        renderHook(() =>
+            useGif(ref, defaultOptions, async (result) => {
+                it("should contain a blob as result", () => {
+                    expect(result).toBeInstanceOf(Blob); // image/gif blob
+                });
+            })
+        );
+
+    renderHelper({ ref: target });
+});
